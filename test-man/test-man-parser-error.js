496cd24f73e33e296e7aab2f98407aa2ad9e9697
yoko0180 <yoko0180@gmail.com>
2020-09-26 01:58:32 +0900

    tidy

--- test-man/test-man-parser-error.js
+++
@@ -1,9 +0,0 @@
-const path = require('path')
-const fs = require('fs')
-var request = require('request');
-// const url = 'https://feedly.com/i/subscription/feed%2Fhttp%3A%2F%2Fhamusoku.com%2Findex.rdf'
-// request(url).pipe(fs.createWriteStream('hamu.txt'))
-
-const url = 'http://b.hatena.ne.jp/entrylist?sort=hot&threshold=&mode=rss'
-request(url).pipe(fs.createWriteStream('hatena.txt'))
-
